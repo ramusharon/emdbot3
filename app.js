6a487a44e5984a6459bcbf612f8a9267b31ca15a
ranga Rajagopalan <33202653+ramusharon@users.noreply.github.com>
2017-11-02 05:54:02 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,109 +1,47 @@
-// This loads the environment variables from the .env file
-require('dotenv-extended').load();
+/*-----------------------------------------------------------------------------
+A simple echo bot for the Microsoft Bot Framework. 
+-----------------------------------------------------------------------------*/
 
-var builder = require('botbuilder');
 var restify = require('restify');
-var Promise = require('bluebird');
-var url = require('url');
-var Swagger = require('swagger-client');
-
-// Swagger client for Bot Connector API
-var connectorApiClient = new Swagger({
-    url: 'https://raw.githubusercontent.com/Microsoft/BotBuilder/master/CSharp/Library/Microsoft.Bot.Connector.Shared/Swagger/ConnectorAPI.json',
-    usePromise: true
-});
+var builder = require('botbuilder');
+var builder_cognitiveservices = require("botbuilder-cognitiveservices");
 
 // Setup Restify Server
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
-    console.log('%s listening to %s', server.name, server.url);
+   console.log('%s listening to %s', server.name, server.url); 
 });
-
-// Create chat bot
+  
+// Create chat connector for communicating with the Bot Framework Service
 var connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
+    appId: process.env.MicrosoftAppId,
+    appPassword: process.env.MicrosoftAppPassword,
+    stateEndpoint: process.env.BotStateEndpoint,
+    openIdMetadata: process.env.BotOpenIdMetadata 
 });
 
-// Listen for messages
+// Listen for messages from users 
 server.post('/api/messages', connector.listen());
 
-// Bot setup
-var bot = new builder.UniversalBot(connector, function (session) {
-    var message = session.message;
-    var conversationId = message.address.conversation.id;
+/*----------------------------------------------------------------------------------------
+* Bot Storage: This is a great spot to register the private state storage for your bot. 
+* We provide adapters for Azure Table, CosmosDb, SQL Azure, or you can implement your own!
+* For samples and documentation, see: https://github.com/Microsoft/BotBuilder-Azure
+* ---------------------------------------------------------------------------------------- */
 
-    // when a group conversation message is recieved,
-    // get the conversation members using the REST API and print it on the conversation.
+// Create your bot with a function to receive messages from the user
+var bot = new builder.UniversalBot(connector);
 
-    // 1. inject the JWT from the connector to the client on every call
-    addTokenToClient(connector, connectorApiClient).then(function (client) {
-        // 2. override API client host and schema (https://api.botframework.com) with channel's serviceHost (e.g.: https://slack.botframework.com or http://localhost:NNNN)
-        var serviceUrl = url.parse(message.address.serviceUrl);
-        var serviceScheme = serviceUrl.protocol.split(':')[0];
-        client.setSchemes([serviceScheme]);
-        client.setHost(serviceUrl.host);
-        // 3. GET /v3/conversations/{conversationId}/members
-        return client.Conversations.Conversations_GetConversationMembers({ conversationId: conversationId })
-            .then(function (res) {
-                printMembersInChannel(message.address, res.obj);
-            });
-    }).catch(function (error) {
-        console.log('Error retrieving conversation members', error);
-    });
-});
 
-bot.on('conversationUpdate', function (message) {
-    if (message.membersAdded && message.membersAdded.length > 0) {
-        var membersAdded = message.membersAdded
-            .map(function (m) {
-                var isSelf = m.id === message.address.bot.id;
-                return (isSelf ? message.address.bot.name : m.name) || '' + ' (Id: ' + m.id + ')';
-            })
-            .join(', ');
+var recognizer = new builder_cognitiveservices.QnAMakerRecognizer({
+                knowledgeBaseId: process.env.QnAKnowledgebaseId, 
+    subscriptionKey: process.env.QnASubscriptionKey});
 
-        bot.send(new builder.Message()
-            .address(message.address)
-            .text('Welcome ' + membersAdded));
-    }
+var basicQnAMakerDialog = new builder_cognitiveservices.QnAMakerDialog({
+    recognizers: [recognizer],
+                defaultMessage: 'No match! Try changing the query terms!',
+                qnaThreshold: 0.3}
+);
 
-    if (message.membersRemoved && message.membersRemoved.length > 0) {
-        var membersRemoved = message.membersRemoved
-            .map(function (m) {
-                var isSelf = m.id === message.address.bot.id;
-                return (isSelf ? message.address.bot.name : m.name) || '' + ' (Id: ' + m.id + ')';
-            })
-            .join(', ');
 
-        bot.send(new builder.Message()
-            .address(message.address)
-            .text('The following members ' + membersRemoved + ' were removed or left the conversation :('));
-    }
-});
-
-// Helper methods
-
-// Inject the connector's JWT token into to the Swagger client
-function addTokenToClient(connector, clientPromise) {
-    // ask the connector for the token. If it expired, a new token will be requested to the API
-    var obtainToken = Promise.promisify(connector.getAccessToken.bind(connector));
-    return Promise.all([obtainToken(), clientPromise]).then(function (values) {
-        var token = values[0];
-        var client = values[1];
-        client.clientAuthorizations.add('AuthorizationBearer', new Swagger.ApiKeyAuthorization('Authorization', 'Bearer ' + token, 'header'));
-        return client;
-    });
-}
-
-// Create a message with the member list and send it to the conversationAddress
-function printMembersInChannel(conversationAddress, members) {
-    if (!members || members.length === 0) return;
-
-    var memberList = members.map(function (m) { return '* ' + m.name + ' (Id: ' + m.id + ')'; })
-        .join('\n ');
-
-    var reply = new builder.Message()
-        .address(conversationAddress)
-        .text('These are the members of this conversation: \n ' + memberList);
-    bot.send(reply);
-}
+bot.dialog('/', basicQnAMakerDialog);
