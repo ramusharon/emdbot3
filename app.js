0d1172a3a6907a8021189c499941ac66801f60f5
ranga Rajagopalan <33202653+ramusharon@users.noreply.github.com>
2017-11-02 05:51:39 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
+// This loads the environment variables from the .env file
+require('dotenv-extended').load();
+
 var builder = require('botbuilder');
 var restify = require('restify');
+var Promise = require('bluebird');
+var url = require('url');
+var Swagger = require('swagger-client');
+
+// Swagger client for Bot Connector API
+var connectorApiClient = new Swagger({
+    url: 'https://raw.githubusercontent.com/Microsoft/BotBuilder/master/CSharp/Library/Microsoft.Bot.Connector.Shared/Swagger/ConnectorAPI.json',
+    usePromise: true
+});
 
 // Setup Restify Server
 var server = restify.createServer();
@@ -13,43 +25,85 @@ var connector = new builder.ChatConnector({
     appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
 
-const ChangePasswordOption = 'Change Password';
-const ResetPasswordOption = 'Reset Password';
-
-var bot = new builder.UniversalBot(connector, [
-    (session) => {
-        builder.Prompts.choice(session,
-            'What do yo want to do today?',
-            [ChangePasswordOption, ResetPasswordOption],
-            { listStyle: builder.ListStyle.button });
-    },
-    (session, result) => {
-        if (result.response) {
-            switch (result.response.entity) {
-                case ChangePasswordOption:
-                    session.send('This functionality is not yet implemented! Try resetting your password.');
-                    session.reset();
-                    break;
-                case ResetPasswordOption:
-                    session.beginDialog('resetPassword:/');
-                    break;
-            }
-        } else {
-            session.send(`I am sorry but I didn't understand that. I need you to select one of the options below`);
-        }
-    },
-    (session, result) => {
-        if (result.resume) {
-            session.send('You identity was not verified and your password cannot be reset');
-            session.reset();
-        }
-    }
-]);
-
-//Sub-Dialogs
-bot.library(require('./dialogs/reset-password'));
-
-//Validators
-bot.library(require('./validators'));
-
+// Listen for messages
 server.post('/api/messages', connector.listen());
+
+// Bot setup
+var bot = new builder.UniversalBot(connector, function (session) {
+    var message = session.message;
+    var conversationId = message.address.conversation.id;
+
+    // when a group conversation message is recieved,
+    // get the conversation members using the REST API and print it on the conversation.
+
+    // 1. inject the JWT from the connector to the client on every call
+    addTokenToClient(connector, connectorApiClient).then(function (client) {
+        // 2. override API client host and schema (https://api.botframework.com) with channel's serviceHost (e.g.: https://slack.botframework.com or http://localhost:NNNN)
+        var serviceUrl = url.parse(message.address.serviceUrl);
+        var serviceScheme = serviceUrl.protocol.split(':')[0];
+        client.setSchemes([serviceScheme]);
+        client.setHost(serviceUrl.host);
+        // 3. GET /v3/conversations/{conversationId}/members
+        return client.Conversations.Conversations_GetConversationMembers({ conversationId: conversationId })
+            .then(function (res) {
+                printMembersInChannel(message.address, res.obj);
+            });
+    }).catch(function (error) {
+        console.log('Error retrieving conversation members', error);
+    });
+});
+
+bot.on('conversationUpdate', function (message) {
+    if (message.membersAdded && message.membersAdded.length > 0) {
+        var membersAdded = message.membersAdded
+            .map(function (m) {
+                var isSelf = m.id === message.address.bot.id;
+                return (isSelf ? message.address.bot.name : m.name) || '' + ' (Id: ' + m.id + ')';
+            })
+            .join(', ');
+
+        bot.send(new builder.Message()
+            .address(message.address)
+            .text('Welcome ' + membersAdded));
+    }
+
+    if (message.membersRemoved && message.membersRemoved.length > 0) {
+        var membersRemoved = message.membersRemoved
+            .map(function (m) {
+                var isSelf = m.id === message.address.bot.id;
+                return (isSelf ? message.address.bot.name : m.name) || '' + ' (Id: ' + m.id + ')';
+            })
+            .join(', ');
+
+        bot.send(new builder.Message()
+            .address(message.address)
+            .text('The following members ' + membersRemoved + ' were removed or left the conversation :('));
+    }
+});
+
+// Helper methods
+
+// Inject the connector's JWT token into to the Swagger client
+function addTokenToClient(connector, clientPromise) {
+    // ask the connector for the token. If it expired, a new token will be requested to the API
+    var obtainToken = Promise.promisify(connector.getAccessToken.bind(connector));
+    return Promise.all([obtainToken(), clientPromise]).then(function (values) {
+        var token = values[0];
+        var client = values[1];
+        client.clientAuthorizations.add('AuthorizationBearer', new Swagger.ApiKeyAuthorization('Authorization', 'Bearer ' + token, 'header'));
+        return client;
+    });
+}
+
+// Create a message with the member list and send it to the conversationAddress
+function printMembersInChannel(conversationAddress, members) {
+    if (!members || members.length === 0) return;
+
+    var memberList = members.map(function (m) { return '* ' + m.name + ' (Id: ' + m.id + ')'; })
+        .join('\n ');
+
+    var reply = new builder.Message()
+        .address(conversationAddress)
+        .text('These are the members of this conversation: \n ' + memberList);
+    bot.send(reply);
+}
