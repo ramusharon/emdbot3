d8da555ebcdaa976a8fec90774bd4901b3a6ea25
ranga Rajagopalan <33202653+ramusharon@users.noreply.github.com>
2017-11-07 06:21:14 -0500

    Update app.js

--- app.js
+++ app.js
@@ -33,15 +33,22 @@ var bot = new builder.UniversalBot(connector, [
         session.dialogData.reservationDate = builder.EntityRecognizer.resolveTime([results.response]);
         builder.Prompts.number(session, "How many people are in your party?");
     },
+      
     function (session, results) {
         session.dialogData.partySize = results.response;
         builder.Prompts.text(session, "Whose name will this reservation be under?");
     },
-    function (session, results) {
+   
+   function (session, results) {
         session.dialogData.reservationName = results.response;
+        builder.Prompts.text(session, "What is the name of the resturant location?");
+   },
+   
+    function (session, results) {       
+        session.dialogData.ResturantLocation = results.response;
 
         // Process request and display reservation details
-        session.send(`Reservation confirmed. Reservation details: <br/>Date/Time: ${session.dialogData.reservationDate} <br/>Party size: ${session.dialogData.partySize} <br/>Reservation name: ${session.dialogData.reservationName}`);
+        session.send(`Reservation confirmed. Reservation details: <br/>Date/Time: ${session.dialogData.reservationDate} <br/>Party size: ${session.dialogData.partySize} <br/>Reservation name: ${session.dialogData.reservationName} <br/>Resturant Location: ${session.dialogData.ResturantLocation}`);
         session.endDialog();
     }
 ]);
