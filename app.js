a51536a131f97a3ff53cb93a0a907ab881764c8f
Rangaraman Rajagopalan <M144752@na.merckgroup.com>
2017-11-07 12:46:36 -0500

    12:46

--- app.js
+++ app.js
@@ -61,9 +61,9 @@ bot.dialog('getSalesData', [
 ]);
 
 // The dialog stack is cleared and this dialog is invoked when the user enters 'help'.
-bot.dialog('help', function (session, args, next) {
+bot.dialog('tryagain', function (session, args, next) {
     session.endDialog("This is a bot that can help you make a dinner reservation. <br/>Please say 'next' to continue");
 })
 .triggerAction({
-    matches: /^help$/i,
+    matches: /^tryagain$/i,
 });
