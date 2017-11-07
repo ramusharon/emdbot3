45d0624bcd8c3b27c5d3e9238e92705030f47be0
Rangaraman Rajagopalan <M144752@na.merckgroup.com>
2017-11-07 12:07:41 -0500

    12:07

--- app.js
+++ app.js
@@ -32,13 +32,18 @@ var bot = new builder.UniversalBot(connector, [
 ]);
 
 bot.dialog('greetings', [
-    // Step 1
+    function (session) {
+        session.beginDialog('askName');
+    },
+    function (session, results) {
+        session.endDialog('Hello %s!', results.response);
+    }
+]);
+bot.dialog('askName', [
     function (session) {
         builder.Prompts.text(session, 'Hi! What is your name?');
     },
-    // Step 2
     function (session, results) {
-        session.endDialog(`Hello ${results.response}!`);
+        session.endDialogWithResult(results);
     }
 ]);
-
