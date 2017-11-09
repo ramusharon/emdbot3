27ca37994cd8077f174f4d872e93ad76667701f5
ramusharon <33202653+ramusharon@users.noreply.github.com>
2017-11-08 23:36:41 -0500

    11:36

--- app.js
+++ app.js
@@ -15,25 +15,44 @@ their choice to the list.
 * For samples and documentation, see: https://github.com/Microsoft/BotBuilder-Azure
 * ---------------------------------------------------------------------------------------- */
 
-require('dotenv-extended').load();
-const restify = require('restify');
-const builder = require('botbuilder');
-const bot = require('./bot.js');
+/*-----------------------------------------------------------------------------
+This Bot demonstrates how to create a custom prompt the conditionally uses a 
+choice() prompt. The user can either pick an option from the list of choices 
+or enter a new choice.  The sample also shows how to prompt the user to add
+their choice to the list.
+# RUN THE BOT:
+    Run the bot from the command line using "node app.js" and then type 
+    "hello" to wake the bot up.
+    
+-----------------------------------------------------------------------------*/
 
-const server = restify.createServer();
-server.post('/api/messages', bot.connector('*').listen());
-server.listen(process.env.PORT || 3978, () => {
-    console.log(`${server.name} listening to ${server.url}`);
+/*----------------------------------------------------------------------------------------
+* Bot Storage: This is a great spot to register the private state storage for your bot. 
+* We provide adapters for Azure Table, CosmosDb, SQL Azure, or you can implement your own!
+* For samples and documentation, see: https://github.com/Microsoft/BotBuilder-Azure
+* ---------------------------------------------------------------------------------------- */
+
+
+var restify = require('restify');
+var builder = require('botbuilder');
+var savedListPrompt = require('./savedListPrompt');
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+   console.log('%s listening to %s', server.name, server.url); 
 });
-
-
-
+  
+// Create chat connector for communicating with the Bot Framework Service
 var connector = new builder.ChatConnector({
     appId: process.env.MicrosoftAppId,
     appPassword: process.env.MicrosoftAppPassword,
     stateEndpoint: process.env.BotStateEndpoint,
     openIdMetadata: process.env.BotOpenIdMetadata 
 });
+
+// Listen for messages from users 
+server.post('/api/messages', connector.listen());
+
 
 // In a bot, a conversation can hold a collection of dialogs.
 
