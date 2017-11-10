572a0fde0e5b582f005d3d1081c89112828dade8
ramusharon <33202653+ramusharon@users.noreply.github.com>
2017-11-10 11:00:57 -0500

    Commit 11:00

--- app.js
+++ app.js
@@ -1,38 +1,3 @@
-/*-----------------------------------------------------------------------------
-This Bot demonstrates how to create a custom prompt the conditionally uses a 
-choice() prompt. The user can either pick an option from the list of choices 
-or enter a new choice.  The sample also shows how to prompt the user to add
-their choice to the list.
-# RUN THE BOT:
-    Run the bot from the command line using "node app.js" and then type 
-    "hello" to wake the bot up.
-    
------------------------------------------------------------------------------*/
-
-/*----------------------------------------------------------------------------------------
-* Bot Storage: This is a great spot to register the private state storage for your bot. 
-* We provide adapters for Azure Table, CosmosDb, SQL Azure, or you can implement your own!
-* For samples and documentation, see: https://github.com/Microsoft/BotBuilder-Azure
-* ---------------------------------------------------------------------------------------- */
-
-/*-----------------------------------------------------------------------------
-This Bot demonstrates how to create a custom prompt the conditionally uses a 
-choice() prompt. The user can either pick an option from the list of choices 
-or enter a new choice.  The sample also shows how to prompt the user to add
-their choice to the list.
-# RUN THE BOT:
-    Run the bot from the command line using "node app.js" and then type 
-    "hello" to wake the bot up.
-    
------------------------------------------------------------------------------*/
-
-/*----------------------------------------------------------------------------------------
-* Bot Storage: This is a great spot to register the private state storage for your bot. 
-* We provide adapters for Azure Table, CosmosDb, SQL Azure, or you can implement your own!
-* For samples and documentation, see: https://github.com/Microsoft/BotBuilder-Azure
-* ---------------------------------------------------------------------------------------- */
-
-
 var restify = require('restify');
 var builder = require('botbuilder');
 var savedListPrompt = require('./savedListPrompt');
