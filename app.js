f294e54a498c2227363c4f83607df456ee05596d
ramusharon <33202653+ramusharon@users.noreply.github.com>
2017-11-10 11:19:06 -0500

    11:18

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
+/*-----------------------------------------------------------------------------
+This Bot demonstrates how to create a custom prompt the conditionally uses a 
+choice() prompt. The user can either pick an option from the list of choices 
+or enter a new choice.  The sample also shows how to prompt the user to add
+their choice to the list.
+# RUN THE BOT:
+    Run the bot from the command line using "node app.js" and then type 
+    "hello" to wake the bot up.
+    
+-----------------------------------------------------------------------------*/
+
+/*----------------------------------------------------------------------------------------
+* Bot Storage: This is a great spot to register the private state storage for your bot. 
+* We provide adapters for Azure Table, CosmosDb, SQL Azure, or you can implement your own!
+* For samples and documentation, see: https://github.com/Microsoft/BotBuilder-Azure
+* ---------------------------------------------------------------------------------------- */
+
+
 var restify = require('restify');
 var builder = require('botbuilder');
-//var savedListPrompt = require('./savedListPrompt');
+var savedListPrompt = require('./savedListPrompt');
 // Setup Restify Server
 var server = restify.createServer();
-
 server.listen(process.env.port || process.env.PORT || 3978, function () {
    console.log('%s listening to %s', server.name, server.url); 
 });
@@ -19,29 +36,21 @@ var connector = new builder.ChatConnector({
 // Listen for messages from users 
 server.post('/api/messages', connector.listen());
 
-// From the examples start
-var meaningOfLife = require('./meaningOfLife'); 
-26 
- 
-27 // Setup bot and define root waterfall 
-28 var connector = new builder.ConsoleConnector().listen(); 
-29 var bot = new builder.UniversalBot(connector, [ 
-30     function (session) { 
-31         // Ask user the meaning of life 
-32         meaningOfLife.beginDialog(session); 
-33     }, 
-34     function (session, results) { 
-35         // Check their answer 
-36         if (results.response) { 
-37             session.send("That's correct! You are wise beyond your years..."); 
-38         } else { 
-39             session.send("Sorry you couldn't figure it out. Everyone knows that the meaning of life is 42."); 
-40         } 
-41     } 
-42 ]); 
-43 
- 
-44 // Create prompts 
-45 meaningOfLife.create(bot); 
 
+var bot = new builder.UniversalBot(connector, [
+    function (session) {
+        // Prompt for message to send
+        savedListPrompt.beginDialog(session, {
+            field: 'savedMessages',
+            choicesPrompt: "What message would you like to send? Choose a saved message from the list or enter a new one.",
+            noChoicesPrompt: "What message would you like to send to us ?"
+        });
+    },
+    function (session, results) {
+        session.send("Sending message: " + results.response);
+    }
+]);
+
+// Create prompts
+savedListPrompt.create(bot);
 
